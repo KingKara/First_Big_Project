34d8ffcbac6c86d3c19e5cc46b9146c59bdaa9a2
KingKara <kinga.kara.93@gmail.com>
2019-04-02 20:46:49 +0200

    Test repo.

---
+++ src/com/kodilla/BattleShips.java
@@ -0,0 +1,34 @@
+package com.kodilla;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class BattleShips extends Application {
+
+    private Image imageback = new Image("file:resources/Background.jpg");
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(200, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
+        primaryStage.setTitle("BattleShips");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
